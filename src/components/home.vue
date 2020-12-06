9a4f2a2a38e468226c001bf92f4793100b9930a3
Jesus Bohorquez <bohorquez866@gmail.com>
2020-12-06 18:46:46 -0500

    new styles

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,25 +1,29 @@
 <template>
   <div class="hello">
-    <h1 class="title has-text-weight-bold">{{ msg }}</h1>
+    <h1 class="title has-text-weight-bold has-text-primary">{{ msg }}</h1>
 
     <section class="section">
       <div class="container">
         <div class>
-          <h2>{{}}</h2>
-          <ul class="cards columns is is-multiline mt-6">
-            <li class="card column is-half mb-3" v-for="commit in commits" :key="commit">
+
+                <ul class="cards is-flex is-justify-content-space-between columns is-variable is-3 is-multiline mt-6">
+            <li
+              class="card is-primary notification column is-one-third  mb-3 is-rounded"
+              v-for="commit in commits"
+              :key="commit"
+            >
               <h2 class="has-text-weight-bold">
                 Author
-                <strong class="has-text-link">{{commit.author.login}}</strong>
+                <strong class="has-text-warning">{{commit.author.login}}</strong>
               </h2>
               <h3 class="has-text-weight-bold">
                 <span>Sha:</span>
-                <strong class="has-text-link">{{commit.sha}}</strong>
+                <strong class="has-text-warning">{{commit.sha}}</strong>
               </h3>
 
               <h3 class="has-text-weight-semibold">
                 Comment:
-                <em>{{commit.commit.message}}</em>
+                <em v-if="commit.commit.message < 1">{{commit.commit.message}}</em>
               </h3>
               <p>
                 <strong>Date:</strong>
@@ -42,7 +46,7 @@ export default {
 
   data: function() {
     return {
-      url: `https://api.github.com/search/commits?q=repo:bohorquez866/github-commit-list author-date:1973-01-01..2021-12-07`,
+      url: `https://api.github.com/search/commits?q=repo:bohorquez866/github-commit-list/master author-date:1973-01-01..2021-12-07`,
       commits: {},
       res: [],
       index: 0
